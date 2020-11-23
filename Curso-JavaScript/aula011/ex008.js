c620c80153e500c8ae23f7a198c01ed31afee15a
Jeferson Lopes <jefersonlopesluis@gmail.com>
2020-11-23 12:16:59 -0300

    Criei pasta
    Criei pasta 11 com dois exemplos

---
+++ Curso-JavaScript/aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ //condição simples
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando sinto de segurança!`)
